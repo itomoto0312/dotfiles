13fd486781c8d316bc1d4cbfcb51d69753ae8cf5
itomoto0312 <itou.motoki0312@gmail.com>
2017-04-09 14:19:25 +0900

    [mod] peco history のaliasを変更

--- fish/alias.fish
+++ fish/alias.fish
@@ -5,7 +5,7 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias tree="find . | grep -v ".git" |sort|sed -ne'1b;s/[^\/]*\//+--/g;s/+--+/| +/g;s/+--+/| +/g;s/+--|/| |/g;p'"
 alias ffind='find ./ -name'
-alias phist='peco_select_history'
+alias hist='peco_select_history'
 alias gs='git status'
 alias gd='git diff .'
 
